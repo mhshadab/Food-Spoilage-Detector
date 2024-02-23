5cacf92db135b0424369e1b02e721bd41bf44904
mhshadab <shadab@ualberta.ca>
2024-02-23 03:49:32 -0700

    All code and supporting files for Food Spoilage Detector

---
+++ SafeFood_model_trainer.py
@@ -0,0 +1,76 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+# Define the path to the images
+data_dir = r'D:\hackathon\testImage'  # Using a raw string
+
+# Set up data augmentation parameters
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest',
+    validation_split=0.2  # Using 20% of the images for validation
+)
+
+# Since we have a very small dataset, we'll use a smaller batch size
+batch_size = 3
+
+# Set up the generators
+train_generator = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='training'  # Set as training data
+)
+
+validation_generator = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(224, 224),
+    batch_size=batch_size,
+    class_mode='categorical',
+    subset='validation'  # Set as validation data
+)
+
+# Load the pre-trained MobileNetV2 model
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+# Freeze the base_model
+base_model.trainable = False
+
+# Add custom layers on top
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(1024, activation='relu')(x)
+predictions = Dense(train_generator.num_classes, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Compile the model
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'], run_eagerly=True)
+
+# Calculate appropriate steps per epoch
+train_steps = train_generator.samples // batch_size
+validation_steps = validation_generator.samples // batch_size
+
+# Train the model
+epochs = 10
+history = model.fit(
+    train_generator,
+    steps_per_epoch=train_steps,
+    validation_data=validation_generator,
+    validation_steps=validation_steps,
+    epochs=epochs
+)
+
+# Save the model for later use
+model.save('veggie_spoilage_predictor.h5')
